956f5261e069ef67263a6daf16becf534a7143d1
sebastian-gott <58074482+sebastian-gott@users.noreply.github.com>
2021-05-22 22:22:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -55,11 +55,10 @@ client.on('ready', () => {
 
   client.on('message', merkurMessage => {
     if(merkurMessage.author.id === "101771139361026048") {
-      try {
+      
         merkurMessage.guild.members.cache.get("504746289372594183").setNickname("Kuk i munn")
-      } catch(err) {
-        merkurMessage.send(err.message)
-      }
+       
+
       
     }
   })
